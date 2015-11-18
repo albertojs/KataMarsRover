b254a5d647ef29a788221a7994caa7a9599570f5
albertojs <ajspollo@hotmail.com>
2015-11-18 20:59:57 +0100

    #refactor: move Point tests to another describe

--- tests/marsRoverSpec.js
+++ tests/marsRoverSpec.js
@@ -1,26 +1,31 @@
 describe("Mars Rover", function(){
 
-	it("gets X coordinate", function(){
-		var rover = new Rover(new Point(1, 1), Direction.NORTH);
-
-		var coordinateX = rover.point.getX();
-
-		expect(coordinateX).toBe(1);
-	});
-
-	it("gets Y coordinate", function(){
-		var rover = new Rover(new Point(1, 2), Direction.NORTH);
-
-		var coordinateY = rover.point.getY();
-
-		expect(coordinateY).toBe(2);
-	});
-
 	it("gets direction", function(){
 		var rover = new Rover(new Point(1, 2), Direction.SOUTH);
 
 		var direction = rover.direction;
 
 		expect(direction).toBe('S');
+	});
+
+	it
+});
+
+describe("Point", function(){
+
+	it("gets X coordinate", function(){
+		var point = new Point(1, 1);
+
+		var coordinateX = point.getX();
+
+		expect(coordinateX).toBe(1);
+	});
+
+	it("gets Y coordinate", function(){
+		var point = new Point(1, 2);
+
+		var coordinateY = point.getY();
+
+		expect(coordinateY).toBe(2);
 	});
 });
