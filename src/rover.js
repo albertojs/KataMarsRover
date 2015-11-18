eeb14343d842fc74974fa2f862c6c99287f827a2
albertojs <ajspollo@hotmail.com>
2015-11-18 20:49:41 +0100

    #refactor: move to another files

--- src/rover.js
+++ src/rover.js
@@ -4,23 +4,3 @@ function Rover(startingPoint, startingDirection)
 	this.direction = startingDirection;
 	
 }
-
-function Point(x, y)
-{
-	this.getX = function()
-	{
-		return x;
-	};
-
-	this.getY = function()
-	{
-		return y;
-	};
-}
-
-var Direction =	{
-					NORTH: 'N',
-					SOUTH: 'S',
-					EAST: 'E',
-					WEST: 'W'
-				};
