28d2bb3d587392695912b3cc3b0eae4e9b23f3ea
albertojs <ajspollo@hotmail.com>
2015-11-18 20:21:10 +0100

    green gets Y coordinate

--- tests/marsRoverSpec.js
+++ tests/marsRoverSpec.js
@@ -8,12 +8,25 @@ describe("Mars Rover", function(){
 		expect(coordinateX).toBe(1);
 	});
 
+	it("gets Y coordinate", function(){
+		var rover = new Rover(1, 2, 'N');
+
+		var coordinateY = rover.getY();
+
+		expect(coordinateY).toBe(2);
+	});
+
 
 	function Rover(x, y, direction)
 	{
 		this.getX = function()
 		{
 			return x;
+		};
+
+		this.getY = function()
+		{
+			return y;
 		};
 	}
 });
