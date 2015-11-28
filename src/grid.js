a4527ae19b5d11adb7b5ce44b22bb6f3256eff9d
albertojs <ajspollo@hotmail.com>
2015-11-28 02:17:26 +0100

    green generates next north point

--- src/grid.js
+++ src/grid.js
@@ -4,5 +4,10 @@ function Grid(width, height)
 	{
 		if(point.getX() < 0 || point.getX() > width ||point.getY() < 0 || point.getY() >height)
 			throw "Point is out of the Grid";
-	}
+	};
+
+	this.nextNorthPoint = function(point)
+	{
+		return new Point(point.getX(), point.getY() + 1);
+	};
 }
--- tests/gridSpec.js
+++ tests/gridSpec.js
@@ -1,12 +1,22 @@
 describe("Grid", function(){
-	
+	var grid;
+	beforeEach(function(){
+		grid = new Grid(5, 4);
+	});
+
 	it("throws exception when a point is out of the grid", function(){
-		var grid = new Grid(5, 5);
 
 		var validatePoint = function(){
 			grid.validatePoint(new Point(6, 2))
 		};
 
 		expect(validatePoint).toThrow();
+	});
+
+	it("generates next north point", function(){
+		var nextPoint = grid.nextNorthPoint(new Point(2, 3));
+
+		expect(nextPoint.getX()).toBe(2);
+		expect(nextPoint.getY()).toBe(4);
 	});
 });
